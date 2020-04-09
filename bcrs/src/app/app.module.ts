0ef5ab337ac981375571aa6479aca9a48f5ee89b
Justin Singleton <jsingleton@my365.bellevue.edu>
2020-04-09 09:39:36 -0500

    Update dependancies

--- bcrs/src/app/app.module.ts
+++ bcrs/src/app/app.module.ts
@@ -15,7 +15,6 @@ import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { SecurityQuestionCreateComponent } from './pages/security-question-create/security-question-create.component';
-import { SecurityQuestionsListComponent } from './pages/security-questions-list/security-questions-list.component';
 import { SigninComponent } from './pages/signin/signin.component';
 import { UserDetailsComponent } from './pages/user-details/user-details.component';
 import { UserListComponent } from './pages/user-list/user-list.component';
@@ -29,13 +28,6 @@ import { SecurityQuestionDetailsComponent } from './pages/security-question-deta
     AuthLayoutComponent,
     HomeComponent,
     SecurityQuestionCreateComponent,
-<<<<<<< HEAD
-    SecurityQuestionsListComponent,
-    SigninComponent,
-    UserDetailsComponent,
-    UserListComponent,
-=======
->>>>>>> 0a2f8a9b9eb7f33abadab2e286ac314bb30d7434
     SecurityQuestionListComponent,
     SigninComponent,
     UserDetailsComponent,
