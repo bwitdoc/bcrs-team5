00d7160a3b91812325db9a3352d7dde9486faf98
bwitdoc <bwitdoc@gmail.com>
2020-04-15 19:17:01 -0700

    adding forgot-password and registration components

--- bcrs/src/app/pages/account-registration/account-registration.component.ts
+++ bcrs/src/app/pages/account-registration/account-registration.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { CookieService } from 'ngx-cookie-service';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
+
 
 @Component({
   selector: 'app-account-registration',
@@ -6,10 +11,79 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./account-registration.component.css']
 })
 export class AccountRegistrationComponent implements OnInit {
+  securityQuestions: any;
+  form: FormGroup;
+  registrationForm: FormGroup;
+  errorMessage: string;
 
-  constructor() { }
+  constructor(private http: HttpClient, private route: Router, private fb: FormBuilder, private cookieService: CookieService) {
+    this.http.get('/api/security-questions').subscribe(res => {
+      this.securityQuestions = res;
+    }, err => {
+      console.log(err);
+    });
+   }
 
   ngOnInit() {
+    this.registrationForm = new FormGroup({
+      contactInformation: new FormGroup({
+        firstname: new FormControl(null, Validators.required),
+        lastname: new FormControl(null, Validators.required),
+        phoneNumber: new FormControl(null, Validators.required),
+        address: new FormControl(null, Validators.required),
+        email: new FormControl(null, Validators.required)
+      }),
+      securityQuestions: new FormGroup({
+        securityQuestions1: new FormControl(null, Validators.required),
+        securityQuestions2: new FormControl(null, Validators.required),
+        securityQuestions3: new FormControl(null, Validators.required),
+        answerToSecurityQuestion1: new FormControl(null, Validators.required),
+        answerToSecurityQuestion2: new FormControl(null, Validators.required),
+        answerToSecurityQuestion3: new FormControl(null, Validators.required)
+      }),
+      credentials: new FormGroup({
+        username: new FormControl(null, Validators.required),
+        password: new FormControl(null, Validators.required),
+      })
+    });
   }
 
+  register(form) {
+    const contactInformation = form.contactInformation;
+    const securityQuestions = form.securityQuestions;
+    const credentials = form.credentials;
+
+    const selectedSecurityQuestions = [
+      {questionId: securityQuestions.securityQuestion1,
+      answer: securityQuestions.answerToSecurityQuestion1},
+
+      {questionId: securityQuestions.securityQuestion2,
+      answer: securityQuestions.answerToSecurityQuestion2},
+
+      {questionId: securityQuestions.securityQuestion3,
+      answer: securityQuestions.answerToSecurityQuestion3}
+    ];
+
+    this.http.post('/api/session/register', {
+      username: credentials.username,
+      password: credentials.password,
+      firstname: contactInformation.firstname,
+      lastname: contactInformation.lastname,
+      phoneNumber: contactInformation.phoneNumber,
+      address: contactInformation.address,
+      email: contactInformation.email,
+      securityQuestions: selectedSecurityQuestions
+    }).subscribe(res => {
+      if (res['auth']) {
+        this.cookieService.set('isAuthenticated', 'true', 1);
+        this.cookieService.set('username', credentials.username, 1);
+        this.router.navigate(['/']);
+      } else {
+        this.errorMessage = res['text'];
+      }
+    }, err => {
+      console.log(err);
+      this.errorMessage = err;
+    });
+  }
 }
--- bcrs/src/app/pages/forgot-password/forgot-password.component.ts
+++ bcrs/src/app/pages/forgot-password/forgot-password.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FromGroup, Validators, FormGroup } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
+import { ActivatedRoute, Router } from '@angular/router';
+import { CookieService } from 'ngx-cookie-service';
 
 @Component({
   selector: 'app-forgot-password',
@@ -6,10 +10,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./forgot-password.component.css']
 })
 export class ForgotPasswordComponent implements OnInit {
+  isAuthenticated: string;
+  username: string;
+  form: FormGroup;
 
-  constructor() { }
+  constructor(private http: HttpClient, private route: ActivatedRoute, private router: Router, private fb: FormBuilder, private cookieService: CookieService) {
+    this.isAuthenticated = this.route.snapshot.queryParamMap.get('isAuthenticated');
+    this.username = this.route.snapshot.queryParamMap.get('username');
+   }
 
   ngOnInit() {
+    this.form = this.fb.group({
+      password: [null, Validators.compose([Validators.required])]
+    });
   }
 
+  resetPassword() {
+    this.http.post('/api/session/users/' + this.username + './reset-password', {
+      password: this.form.controls['password'].value
+    }).subscribe(res => {
+      this.cookieService.set('sessionuser', this.username, 1);
+      this.router.navigate(['/']);
+    }, err => {
+      console.log(err);
+    });
+  }
 }
