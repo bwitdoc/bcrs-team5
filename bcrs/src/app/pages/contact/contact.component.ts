720174dc77ac02fdd7a01e4dfd47b1f790810a6b
Gabriel Sanchez <gjsanchez@my365.bellevue.edu>
2020-04-19 22:54:01 -0500

    contact-page

--- bcrs/src/app/pages/contact/contact.component.ts
+++ bcrs/src/app/pages/contact/contact.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-contact',
@@ -6,7 +7,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./contact.component.css']
 })
 export class ContactComponent implements OnInit {
-
+  emailFormControl = new FormControl('', [
+    Validators.required,
+    Validators.email,
+  ]);
   constructor() { }
 
   ngOnInit() {
